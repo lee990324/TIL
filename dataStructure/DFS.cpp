492172620bb151671ed4d2d843e11f09457394c5
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2024-12-11 02:01:59 +0900

    add: Graph

--- dataStructure/DFS.cpp
+++ dataStructure/DFS.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "Graph.h"
 using namespace std;
+
+virtual void Graph::DFS()
+{
+    visited = new bool[n];
+    fill(visited, visited + n, false);
+    DFS(0);
+    delete[] visited;
+}
+
+virtual void Graph::DFS(const int v)
+{
+    visited[v] = true;
+    for (each vertex w adjacent to v)
+        if (!visited[w])
+            DFS(w);
+}
 
 int main()
 {
